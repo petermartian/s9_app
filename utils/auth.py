e8ac5b56b0a5d43dfecfcb03320ffa69e70973e3
Peter Martian <petmartian@gmail.com>
2025-05-16 11:24:18 +0100

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -21,7 +21,7 @@ CREDENTIALS = {
     "main": {"admin": "mainpass"},
     "trade_sheet": {"trader": "traderpass"},
     "daily_transaction": {"dailyuser": "dailypass"},
-    "bank_statements": {"banker": "bankpass1"},
+    "bank_statements": {"bankuser": "bankerpass1"},
     "database": {"dbadmin": "dbpass1"},
     "finance": {"financeuser": "financepass"},  # Finance credentials
     "hr": {"hruser": "hrpass"}  # HR credentials
